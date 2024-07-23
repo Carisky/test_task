bc82cc69b6fb0392e71bb96ccae95f456577b028
Yehor <egorkorsun7@gmail.com>
2024-07-23 19:28:51 +0200

    desktop done

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 import Customer from './models/Customer.mjs';
-// Set Pug as the template engine
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
@@ -11,17 +11,18 @@ app.use(express.static('public'));
 const customers = [
     new Customer('John Doe', 'Company A', '123-456-7890', 'john@example.com', 'USA', 'Active'),
     new Customer('Jane Smith', 'Company B', '987-654-3210', 'jane@example.com', 'Canada', 'Inactive'),
-    new Customer('Alice Johnson', 'Company C', '555-555-5555', 'alice@example.com', 'UK', 'Pending'),
+    new Customer('Alice Johnson', 'Company C', '555-555-5555', 'alice@example.com', 'UK', 'Inactive'),
+    new Customer('John Doe', 'Company A', '123-456-7890', 'john@example.com', 'USA', 'Active'),
+    new Customer('Jane Smith', 'Company B', '987-654-3210', 'jane@example.com', 'Canada', 'Inactive'),
+    new Customer('Alice Johnson', 'Company C', '555-555-5555', 'alice@example.com', 'UK', 'Inactive'),
+    new Customer('John Doe', 'Company A', '123-456-7890', 'john@example.com', 'USA', 'Active')
   ];
 
-// Define a route
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Customer List', message: 'Hello Evano', customers });
+  res.render('index', { title: 'Customer List', message: 'Hello Evano 👋🏼,', customers });
 });
 
 
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
